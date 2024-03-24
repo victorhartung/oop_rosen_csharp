94b86f3dd3bba745e1fe0abfd15482f06fb6ca62
Pablo Irineu de Souza <pablo.souza.5@outlook.com.br>
2024-03-24 14:57:18 -0300

    fix: renomear classe e modificar excecao

---
+++ ProjetoTDD/Bhaskara.cs
@@ -0,0 +1,37 @@
+﻿namespace ProjetoTDD
+{
+    public static class Bhaskara
+    {
+        public static List<double> CalcularRaizes(int a, int b, int c)
+        {
+            VerificarValorCoeficienteQuadratico(a);
+
+            var x1 = (-b + Math.Sqrt(CalcularDiscriminante(a, b, c))) / (2 * a);
+            var x2 = (-b - Math.Sqrt(CalcularDiscriminante(a, b, c))) / (2 * a);
+
+            List<double> result = [x1, x2];
+
+            return [.. result.OrderBy(x => x)];
+        }
+
+        private static double CalcularDiscriminante(int a, int b, int c)
+        {
+            var res = Math.Pow(b, 2) - 4 * a * c;
+
+            if (res < 0)
+            {
+                throw new ArithmeticException("Não existe solução real");
+            }
+
+            return res;
+        }
+
+        private static void VerificarValorCoeficienteQuadratico(int a)
+        {
+            if (a == 0)
+            {
+                throw new ArithmeticException("Termo quadrático não pode ser zero");
+            }
+        }
+    }
+}
